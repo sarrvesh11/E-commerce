3a798b466046f459c1d6ee18856415b5c4af4b8b
Sarvesh Vajpayee <121345439+sarrvesh11@users.noreply.github.com>
2024-08-13 14:28:45 +0530

    Add files via upload

---
+++ ecom-proj/src/test/java/com/sarvesh/ecom_proj/EcomProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarvesh.ecom_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
